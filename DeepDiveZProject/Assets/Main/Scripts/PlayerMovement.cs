bb086f98d3287fad9582e4592abd8934a4071963
SpherosGames <jbol1@st.noorderpoort.nl>
2024-11-19 12:57:58 +0100

    Added simple player movement done
    + navmesh to level
    ~ player movement: player now moves to where the mouse has clicked
    + floor layer

--- DeepDiveZProject/Assets/Main/Scripts/PlayerMovement.cs
+++ DeepDiveZProject/Assets/Main/Scripts/PlayerMovement.cs
@@ -1,16 +1,51 @@
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private PlayerInput PlayerInput;
+    [SerializeField] private LayerMask FloorLayer;
+
+    public UnityEvent OnPlayerStandsStill;
+
+    private NavMeshAgent agent;
+
+    private bool isMoving;
+
+    private void Awake()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        PlayerInput.actions["LMB"].performed += (InputAction.CallbackContext ctx) => MouseClicked();
+    }
+
+    private void MouseClicked()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //Shoot ray from camera to where the player should move
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, FloorLayer))
+        {
+            isMoving = true;
+            print("Ray hit somethin");
+            agent.SetDestination(hit.point);
+        }
+    }
+
+    private void Update()
+    {
+        //Call event when player stopped moving
+        if (agent.hasPath == false && isMoving)
+        {
+            isMoving = false;
+            print("Player stopped");
+            OnPlayerStandsStill.Invoke();
+        }
     }
 }
